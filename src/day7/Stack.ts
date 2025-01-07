94bf5eeeb354be908a7ab2776fa3411304a7d807
Narcizo Palioto <narcizo.gabriel2@gmail.com>
2025-01-07 14:33:40 -0300

    Stack: this is the nicest one to code

--- src/day7/Stack.ts
+++ src/day7/Stack.ts
@@ -1,20 +1,40 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+}
+
 export default class Stack<T> {
     public length: number;
 
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = {
+            value: item
+        } as Node<T>;
 
+        node.next = this.head;
+        this.head = node;
+
+        this.length++;
     }
 
     pop(): T | undefined {
-        return undefined;
+        if(!this.head) return undefined;
+        const node = this.head;
+        this.head = this.head.next;
+        node.next = undefined;
+
+        this.length--;
+        return node.value;
     }
 
     peek(): T | undefined {
-        
+        return this.head?.value;
     }
 }
