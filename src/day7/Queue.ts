24d777090f5fb89f9d62f08ee9618fb574fa27d9
Narcizo Palioto <narcizo.gabriel2@gmail.com>
2025-01-07 13:38:09 -0300

    Queue: this one is easy

--- src/day7/Queue.ts
+++ src/day7/Queue.ts
@@ -1,20 +1,54 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = {
+            value: item,
+        } as Node<T>;
 
+        if(!this.head && !this.tail){
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail!.next = node;
+            this.tail = node; 
+        }
+
+        this.length++;
     }
 
     deque(): T | undefined {
-        return undefined;
+        if(this.length === 0) return undefined;
+
+        const node = this.head;
+
+        if(this.length === 1){
+            this.head = undefined;
+            this.tail = undefined;
+        } else {
+            this.head = this.head?.next;    
+        }
+        node!.next = undefined;
+        
+        this.length--;
+
+        return node!.value;
     }
 
     peek(): T | undefined {
-
+        return this.head?.value;
     }  
 }
